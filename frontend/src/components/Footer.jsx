c3021d588b0f8b5fb5c237f00726b27aea8dbf8c
gopalpayghan <gopaldadapayghan@gmail.com>
2026-02-03 10:13:26 +0530

    refactor: standardize component formatting

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,21 +1,24 @@
 // import React from 'react'
-import { assets } from '../assets/assets'
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div className='md:mx-10'>
-      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
-        
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
-          <img className='mb-5 w-40' src={assets.logo} alt="" />
-          <p className='w-full md:w-2/3 text-gray-600 leading-6'>
-            Prescripto is dedicated to simplifying healthcare access through our digital prescription management platform. We connect patients, doctors, and pharmacies to streamline the prescription process, ensuring timely medication delivery while maintaining the highest standards of privacy and security for your health information.
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Prescripto is dedicated to simplifying healthcare access through our
+            digital prescription management platform. We connect patients,
+            doctors, and pharmacies to streamline the prescription process,
+            ensuring timely medication delivery while maintaining the highest
+            standards of privacy and security for your health information.
           </p>
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>COMPANY</p>
-          <ul className='flex flex-col gap-2 text-gray-600'>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>Home</li>
             <li>About us</li>
             <li>Delivery</li>
@@ -24,24 +27,22 @@ const Footer = () => {
         </div>
 
         <div>
-          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-          <ul className='flex flex-col gap-2 text-gray-600'>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
             <li>+1-212-456-7890</li>
             <li>prescripto@gmail.com</li>
           </ul>
         </div>
-
       </div>
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>
+        <p className="py-5 text-sm text-center">
           Copyright 2024 @ Prescripto - All Right Reserved.
         </p>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
